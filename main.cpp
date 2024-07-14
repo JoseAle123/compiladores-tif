860eea147ed6fe6ecd86d903d58cf5be71b84e28
I-Dnda-I <dndamc@gmail.com>
2024-07-14 00:11:40 -0500

    agregando 2d mapa

--- main.cpp
+++ main.cpp
@@ -4,180 +4,227 @@
 #include <iostream>
 #define PI 3.14159265f
 
+using namespace std;
+using namespace sf;
+
+const float lado = 50.f;
 const int gridSize = 8;
+const float xIso=sqrt(2*lado*lado);
+const float yIso=xIso/2.f;
+const float ladoIso=sqrt(xIso*xIso/4.f+yIso*yIso/4.f);
+const float angXY=atan2f(xIso,yIso);
+const float angYX=atan2f(yIso,xIso);
+const Vector2f pos_origin(300.f,150.f); 
 
-const sf::Vector2f pos_origin(300.f,150.f); 
-
-sf::Vector2i calculateGridIndices(const sf::Vector2f& position, float tileX, float tileY, const sf::Vector2f& gridOrigin)
+Vector2i calculateGridIndices(const Vector2f& position, const Vector2f& gridOrigin)
 {
-    // Trasladar la posición a partir del origen de la grilla
-    sf::Vector2f relativePosition = position - gridOrigin;
+    Vector2f relativePosition = position - gridOrigin;
+    cout<<"posicionxy: "<<position.x<<"  "<<position.y<<endl;
+    float ang_c1 = angXY - atan2f(abs(relativePosition.x),relativePosition.y);
+    float ang_c2 = 2.f*angXY-ang_c1;
+    float distX, distY;
+    cout<<ang_c1*180.f/PI<<endl;
+    float hipo = sqrt(relativePosition.x*relativePosition.x + relativePosition.y*relativePosition.y);
 
-    float ang_c1 = 60.f*PI/180.f - std::atan2f(std::abs(relativePosition.x),relativePosition.y);
-    float ang_c2 = 120.f*PI/180.f-ang_c1;
-    float distX,distY;
-    //std::cout<<ang_c<<std::endl;
-    float hipo=std::sqrt(relativePosition.x*relativePosition.x + relativePosition.y*relativePosition.y);
     if (relativePosition.x<=0)
-    {
-        distX= (hipo/sin(60.f*PI/180.f))*sin(ang_c1);
-        distY= (hipo/sin(60.f*PI/180.f))*sin(ang_c2);
+    {   
+        distX= (hipo/sin(PI-2.f*angXY))*sin(ang_c1);
+        distY= (hipo/sin(PI-2.f*angXY))*sin(ang_c2);
     }else
     {
-        distY= (hipo/sin(60.f*PI/180.f))*sin(ang_c1);
-        distX= (hipo/sin(60.f*PI/180.f))*sin(ang_c2);
+        distY= (hipo/sin(PI-2.f*angXY))*sin(ang_c1);
+        distX= (hipo/sin(PI-2.f*angXY))*sin(ang_c2);
         
     }
-    std::cout<<distX<<" "<<distY<<std::endl;
-    std::cout<<tileX<<" "<<tileY<<std::endl;
-    sf::Vector2f transformedPosition(
-        distX / tileX,
-        distY / tileY
+    cout<<"distancia xy isometricas"<<distX<<" "<<distY<<endl;
+    Vector2f transformedPosition(
+        distX / ladoIso,
+        distY / ladoIso
     );
-    std::cout<<std::abs(distX) / tileX<<" "<<std::abs(distY) / tileY<<std::endl;
-    // Calcular los índices
-    int gridX = static_cast<int>(std::floor(transformedPosition.x));
-    int gridY = static_cast<int>(std::floor(transformedPosition.y));
-    
 
-    return sf::Vector2i(gridX, gridY);
+    int posXIso = floor(transformedPosition.x);
+    int posYISo = floor(transformedPosition.y);
+    
+    return Vector2i(posXIso, posYISo);
 }
+
 int main()
 {
     // Crear la ventana
-    sf::RenderWindow window(sf::VideoMode(1000, 600), "Makibot");
+    RenderWindow window(VideoMode(1000, 600), "Makibot");
     window.setFramerateLimit(60);
 
     // Coordenadas de la vista isométrica
-    sf::Transform isoTransform;
+    Transform isoTransform;
     isoTransform.translate(300.f, 150.f);
     isoTransform.scale(1.f, 0.5f);
     isoTransform.rotate(45.f);
 
     // Cargar la textura del piso
-    sf::Texture tileTexturePiso;
-    if (!tileTexturePiso.loadFromFile("images/loza_verde.png"))
+    Texture texturaPiso;
+    if (!texturaPiso.loadFromFile("images/loza_verde.png"))
+    {
+        return -1;
+    }
+    Texture texturaPiso2d;
+    if (!texturaPiso2d.loadFromFile("images/loza_verde2d.png"))
     {
         return -1;
     }
     // Cargar la textura del bloque
-    sf::Texture tileTextureBloque;
-    if (!tileTextureBloque.loadFromFile("images/loza_naranja.png"))
+    Texture texturaBloque;
+    if (!texturaBloque.loadFromFile("images/loza_naranja.png"))
+    {
+        return -1;
+    }
+    Texture texturaBloque2d;
+    if (!texturaBloque2d.loadFromFile("images/loza_naranja2d.png"))
     {
         return -1;
     }
     // Crear el sprite del piso
-    const int gridSize = 8;
-    const float tileSize = 50.f;
-    sf::Sprite tiles[gridSize][gridSize];
+    
+    Sprite tiles[gridSize][gridSize];
     for (int i = 0; i < gridSize; ++i)
     {
         for (int j = 0; j < gridSize; ++j)
         {
-            tiles[i][j].setTexture(tileTexturePiso);
-            tiles[i][j].setPosition(i * tileSize, j * tileSize);
+            tiles[i][j].setTexture(texturaPiso);
+            tiles[i][j].setPosition(i * lado, j * lado);
         }
     }
 
-    // Matrix para la cantidad de bloques
-    int matrix[8][8] = {
+    // matriz3D para la cantidad de bloques
+    int matriz3D[8][8] = {
         {0, 2, 2, 0, 0, 0, 0, 0},
         {0, 0, 2, 0, 0, 0, 0, 0},
         {1, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 3, 0, 0},
         {0, 0, 0, 2, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 7, 0, 0, 0},
         {0, 0, 0, 1, 1, 1, 0, 2}
     };
 
     // Vector de sprites para los bloques
-    std::vector<sf::Sprite> bloques;
-
+    vector<Sprite> bloques;
+    // Vector para los bloques q se sobreponen al makibot
+    vector <Sprite> bloques2;
     // Inicializar los sprites del piso y los bloques
     for (int i = 0; i < gridSize; ++i) {
         for (int j = 0; j < gridSize; ++j) {
-            if (matrix[i][j] != 0) {
-                for (int k = 1; k <= matrix[i][j]; ++k) {
-                    sf::Sprite bloque(tileTextureBloque);
-                    bloque.setPosition(i * tileSize - (8.0f * k), j * tileSize - (8.0f * k));
+            if (matriz3D[i][j] != 0) {
+                for (int k = 1; k <= matriz3D[i][j]; ++k) {
+                    Sprite bloque(texturaBloque);
+                    bloque.setPosition(i * lado - (8.0f * k), j * lado - (8.0f * k));
                     bloques.push_back(bloque);
                 }
             }
         }
     }
+    // Figura del makibot en 2D
+    CircleShape makibot2D;
+    makibot2D.setRadius(5.f); 
+    makibot2D.setFillColor(Color::White); 
+    makibot2D.setOrigin(makibot2D.getRadius(), makibot2D.getRadius());
+    makibot2D.setPosition(507.5f, 407.5f);
+
+    // matriz2D para la cantidad de bloques
+
+    int matriz2D[8][8] = {
+        {0, 1, 1, 0, 0, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 1, 1, 1, 0, 1}
+    };
+    Sprite tiles2d[gridSize][gridSize];
+    for (int i = 0; i < gridSize; ++i)
+    {
+        for (int j = 0; j < gridSize; ++j)
+        {
+            if(matriz2D[i][j] == 1)
+                tiles2d[i][j].setTexture(texturaBloque2d);
+            else
+                tiles2d[i][j].setTexture(texturaPiso2d);
+
+            tiles2d[i][j].setPosition(500 + i * 15,400 + j * 15);
+        }
+    }
 
     // Cargar la textura que contiene todos los sprites
-    sf::Texture spriteSheet;
+    Texture spriteSheet;
     if (!spriteSheet.loadFromFile("images/derecha.png"))
     {
         return -1;
     }
 
     // Crear el sprite de la animación
-    sf::Sprite animatedSprite;
-    animatedSprite.setTexture(spriteSheet);
+    Sprite makibot;
+    makibot.setTexture(spriteSheet);
 
     // Definir los rectángulos para cada fotograma de la animación
     const int frameWidth = 25;
     const int frameHeight = 50;
-    std::vector<sf::IntRect> frames;
-    frames.push_back(sf::IntRect(0, 0, frameWidth, frameHeight));
-    frames.push_back(sf::IntRect(frameWidth, 0, frameWidth, frameHeight));
-    frames.push_back(sf::IntRect(2 * frameWidth, 0, frameWidth, frameHeight));
+    vector<IntRect> frames;
+    frames.push_back(IntRect(0, 0, frameWidth, frameHeight));
+    frames.push_back(IntRect(frameWidth, 0, frameWidth, frameHeight));
+    frames.push_back(IntRect(2 * frameWidth, 0, frameWidth, frameHeight));
 
     // Ajustar la posición inicial del sprite para dibujarse desde la parte inferior
-    animatedSprite.setOrigin(12.5f, 50.f);
-    animatedSprite.setPosition(300.f, 150.f+(50.f/cos(45*PI/180.f))*0.25); // Posición inicial del sprite, el centro del primer cuadrado
-    float des=((50.f/cos(45*PI/180.f))*0.25)/sin(30*PI/180.f);
-    float desy=(50.f/cos(45*PI/180.f))*0.25;
-    float desx=(50.f/cos(45*PI/180.f))*0.5;
+    makibot.setOrigin(12.5f, 50.f);
+    makibot.setPosition(300.f, 150.f + yIso/2.f); // Posición inicial del sprite, el centro del primer cuadrado
+    
 
     // Variables para la animación
     int currentFrame = 0;
-    sf::Clock animationClock;
+    Clock animationClock;
     float animationSpeed = 0.3f; // Aumentar la velocidad de la animación
 
     // Variables para controlar el movimiento
     bool moving = false;
-    sf::Vector2f targetPosition = animatedSprite.getPosition();
+    Vector2f targetPosition = makibot.getPosition();
     bool facingRight = true;
 
+    
     // Bucle principal
     while (window.isOpen())
     {
-        // Manejo de eventos
-        sf::Event event;
+        
+        Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
-            if (event.type == sf::Event::KeyPressed && !moving)
+            if (event.type == Event::KeyPressed && !moving)
             {
-                if (event.key.code == sf::Keyboard::Left)
+                if (event.key.code == Keyboard::Left)
                 {
-                    targetPosition.x -= desx;
-                    targetPosition.y += desy;
+                    targetPosition.x -= xIso/2.f;
+                    targetPosition.y += yIso/2.f;
                     facingRight = false;
                     moving = true;
                 }
-                else if (event.key.code == sf::Keyboard::Right)
+                else if (event.key.code == Keyboard::Right)
                 {
-                    targetPosition.x += desx;
-                    targetPosition.y -= desy;
+                    targetPosition.x += xIso/2.f;
+                    targetPosition.y -= yIso/2.f;
                     facingRight = true;
                     moving = true;
                 }
-                else if (event.key.code == sf::Keyboard::Up)
+                else if (event.key.code == Keyboard::Up)
                 {
-                    targetPosition.x -= desx;
-                    targetPosition.y -= desy;
+                    targetPosition.x -= xIso/2.f;
+                    targetPosition.y -= yIso/2.f;
                     moving = true;
                 }
-                else if (event.key.code == sf::Keyboard::Down)
+                else if (event.key.code == Keyboard::Down)
                 {
-                    targetPosition.x += desx;
-                    targetPosition.y += desy;
+                    targetPosition.x += xIso/2.f;
+                    targetPosition.y += yIso/2.f;
                     moving = true;
                 }
             }
@@ -187,62 +234,84 @@ int main()
         if (moving)
         {
             // Verificar colisión con bloques
-            sf::Vector2f newPosition = targetPosition;
-            int gridX,gridY;
-            sf::Vector2i indices = calculateGridIndices(newPosition,(50.f/cos(45*PI/180.f))*0.5f,(50.f/cos(45*PI/180.f))*0.5f,pos_origin);
-            gridX= indices.x;
-            gridY= indices.y;
-            std::cout<<gridX<<gridY<<std::endl;
-            if (gridX >= 0 && gridX < gridSize && gridY >= 0 && gridY < gridSize && matrix[gridX][gridY] != 0)
+            Vector2f newPosition = targetPosition;
+            int posXIso,posYISo;
+            Vector2i indices = calculateGridIndices(newPosition, pos_origin);
+
+            posXIso = indices.x;
+            posYISo = indices.y;
+
+            cout<<posXIso<<posYISo<<endl;
+            if (!(posXIso >= 0 && posXIso < gridSize && posYISo >= 0 && posYISo < gridSize) || matriz3D[posXIso][posYISo] != 0)
             {
                 moving = false; // Si hay un bloque en la nueva posición, cancelar el movimiento
-                targetPosition = animatedSprite.getPosition(); // Mantener la posición actual
+                targetPosition = makibot.getPosition(); // Mantener la posición actual
             }
             else
             {
                 if (animationClock.getElapsedTime().asSeconds() > animationSpeed)
                 {
                     currentFrame = (currentFrame + 1) % frames.size();
-                    animatedSprite.setTextureRect(frames[currentFrame]);
+                    makibot.setTextureRect(frames[currentFrame]);
                     animationClock.restart();
                 }
 
-                sf::Vector2f currentPosition = animatedSprite.getPosition();
-                sf::Vector2f movement(0.f, 0.f);
+                Vector2f currentPosition = makibot.getPosition();
+                Vector2f movement(0.f, 0.f);
 
                 if (targetPosition.x > currentPosition.x)
-                    movement.x = desx / 50.f;
+                    movement.x = xIso/2.f / 50.f;
                 else if (targetPosition.x < currentPosition.x)
-                    movement.x = -desx / 50.f;
+                    movement.x = -xIso/2.f / 50.f;
 
                 if (targetPosition.y > currentPosition.y)
-                    movement.y = desy / 50.f;
+                    movement.y = yIso/2.f / 50.f;
                 else if (targetPosition.y < currentPosition.y)
-                    movement.y = -desy / 50.f;
+                    movement.y = -yIso/2.f / 50.f;
 
-                animatedSprite.move(movement);
+                makibot.move(movement);
 
                 // Detener el movimiento al alcanzar el objetivo
-                if (std::abs(targetPosition.x - currentPosition.x) < 1.f && std::abs(targetPosition.y - currentPosition.y) < 1.f)
+                if (abs(targetPosition.x - currentPosition.x) < 1.f && abs(targetPosition.y - currentPosition.y) < 1.f)
                 {
                     moving = false;
                     currentFrame = 0;
-                    animatedSprite.setPosition(targetPosition);
-                    animatedSprite.setTextureRect(frames[currentFrame]);
+                    makibot.setPosition(targetPosition);
+                    makibot.setTextureRect(frames[currentFrame]);
                 }
-                //std::cout<<animatedSprite.getPosition().x<<" "<<animatedSprite.getPosition().y<<std::endl;
+                //posicion de makibot2D
+                makibot2D.setPosition(507.5f + 15.f*posXIso ,407.4f + 15.f*posYISo);
+                
+                //mapa 2D , se verifica la posicion de los bloques y el makibot
+                bloques2.clear();
+                for (int i = 0; i < gridSize; ++i) {
+                    for (int j = 0; j < gridSize; ++j) {
+                        if (posXIso<i || posYISo<j) {
+                            for (int k = 1; k <= matriz3D[i][j]; ++k) {
+                                Sprite bloque(texturaBloque);
+                                bloque.setPosition(i * lado - (8.0f * k), j * lado - (8.0f * k));
+                                bloques2.push_back(bloque);
+                            }
+                        }
+                    }
+                }
             }
         }
 
-        // Reflejar el sprite según la dirección
         if (facingRight)
-            animatedSprite.setScale(1.f, 1.f);
+            makibot.setScale(1.f, 1.f);
         else
-            animatedSprite.setScale(-1.f, 1.f);
+            makibot.setScale(-1.f, 1.f);
 
-        // Renderizar
-        window.clear(sf::Color::White);
-
+        window.clear(Color::White);
+        // dibujar piso 2d
+        for (int i = 0; i < gridSize; ++i)
+        {
+            for (int j = 0; j < gridSize; ++j)
+            {
+                window.draw(tiles2d[i][j]);
+            }
+        }
         // Dibujar el piso y los bloques
         for (int i = 0; i < gridSize; ++i)
         {
@@ -256,8 +325,13 @@ int main()
             window.draw(bloque, isoTransform);
         }
 
-        // Dibujar el sprite animado sin la transformación isométrica
-        window.draw(animatedSprite);
+        window.draw(makibot);
+        window.draw(makibot2D);
+
+        for (auto& bloque : bloques2)
+        {
+            window.draw(bloque, isoTransform);
+        }
         
         window.display();
     }
